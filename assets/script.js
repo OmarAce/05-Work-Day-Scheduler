53cd3743e67f6da282bab9ef2d13ff379c8e605a
Ace <omarace95@gmail.com>
2021-10-20 02:52:06 -0700

    added saving to local storage, so far only saving 1 object, however having trouble rendering data to page saving current progress

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var currentDay = $("#currentDay");
 var dayPlanner = $(".planner");
 var timeRow = $(".time-row");
 var currentHour = dayjs().format("H");
-var planItems = [];
+var toDoItems = [];
 
 //Updates Current Date on Page using Dayjs
 
@@ -14,17 +14,12 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-var todos = {
-    9: "get shopping list",
-    12: "eat lunch",
-}
-
 // Dynamically Renders Planner 
 
 function render() {
     for (var i = 9; i < 18; i++) {
       $(dayPlanner).append(`
-      <div class="time-row row" data-hour= ${i}>
+      <div class="time-row row" data-hour=${i}>
         <div class="hour col-2 col-lg-1 d-flex justify-content-center align-items-center">${
           i < 12 ? `${i} am` : i > 12 ? `${i - 12} pm` : "12 pm"
         }</div>
@@ -32,14 +27,36 @@ function render() {
         class="col -8 col-lg-10 description ${
           i < currentHour ? "past" : i === currentHour ? "present" : "future"
         }"
-        >${todos[i] || ""}</textarea
+        >${toDoItems[i] || ""}</textarea
         ><button class="col-2 col-lg-1 btn btn-block d-flex justify-content-center align-items-center saveBtn" id="${i}">
         <i class="fas fa-save"></i>
         </button>
         </div>
         `);
     }
-  }
+}
 
-  render();
-  //when saving todo, update localStorage
+render();
+
+function getTodos(){
+    toDoItems = JSON.parse(localStorage.getItem("toDos"));
+    console.log(toDoItems);
+}
+
+getTodos();
+
+function saveTodo(){
+    var currentRow = parseInt($(this).parent().attr("data-hour")); //Target Curent Row Hour ID Make Integer
+    var taskUpdate = (($(this).parent()).children("textarea")).val(); //Target Text Area
+        toDoItems = {
+        hour: currentRow,
+        text: taskUpdate,
+        }
+    localStorage.setItem("toDos", JSON.stringify(toDoItems));
+    console.log(currentRow);
+    console.log(taskUpdate);
+    getTodos();
+
+}
+
+dayPlanner.on("click", "button", saveTodo);
